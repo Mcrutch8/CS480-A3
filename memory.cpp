d614d81cc8339369898409997364e1760e5b5419
John crutchfield <jcrutchfield0164@sdsu.edu>
2022-06-28 21:28:22 -0700

    added memory file

---
+++ memory.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+struct Node {
+   int data;
+   struct Node *prev;
+   struct Node *next;
+};
+struct Node* head = NULL;
+
+
+
+void AddToMemory(int newdata) {
+   struct Node* newnode = (struct Node*) malloc(sizeof(struct Node));
+   newnode->data = newdata;
+   newnode->prev = NULL;
+   newnode->next = head;
+   if(head != NULL)
+   head->prev = newnode ;
+   head = newnode;
+}
+
+
+void ShowMemory() {
+   struct Node* ptr;
+   ptr = head;
+   while(ptr != NULL) {
+      cout<< ptr->data <<" ";
+      ptr = ptr->next;
+   }
+
+
+}
+
+int main() {
+    for(int i = 0; i < 256; i++) {
+        AddToMemory(2);
+    }
+    
+   cout<<"The doubly linked list is: ";
+   ShowMemory();
+   return 0;
+}
