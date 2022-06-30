e971eae2f2f57ed4088724f34c0168e891fc8f91
John crutchfield <jcrutchfield0164@sdsu.edu>
2022-06-29 20:31:09 -0700

    deallocation method created

--- RequestGeneration.cpp
+++ RequestGeneration.cpp
@@ -28,7 +28,7 @@ class requestGeneration {
         void allocationRequest() {
             vector<int> procID;
             vector<int> memUnits;
-            
+
             // generating 100 request, each request has a process id and a number of memory units requested
             for(int i = 0; i < 100; i++) {
                 procID.push_back(setID());
@@ -36,7 +36,17 @@ class requestGeneration {
             }
 
         }
-
+        void deallocationRequest() {
+            // get a random process id to deallocate 
+            int num;
+	        srand(time(0));
+		    num = rand() % 99 + 1;
+            vector<int> deallocateID;
+            // sepcify the process id that needs to be deallocated
+            for(int i = 0; i < 100; i++) {
+                deallocateID.push_back(num);
+            } 
+        }
 
 
 };
