90abb67f33a05d606abd8f9b1051bad06498ee15
John crutchfield <jcrutchfield0164@sdsu.edu>
2022-06-29 21:12:15 -0700

    comments

---
+++ firstFit.cpp
@@ -0,0 +1,6 @@
+// 1. for the first fit algorithm, we need to grab our memory blocks of 256KB 
+//seperated into 2KB blocks from our memory.cpp
+
+//2. get our allocation and dealocation request from the requestGeneration.cpp
+
+//3. perform first fit algorithm with the request given 
